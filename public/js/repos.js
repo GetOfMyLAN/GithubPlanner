92982c52eabf0ab962b227b0bf59206494406472
GitOffMyLAN <jackpsthefirst@gmail.com>
2018-04-06 18:35:35 +1000

    using angularJS, and also reset javascript

--- public/js/repos.js
+++ public/js/repos.js
@@ -1,28 +1,6 @@
-var username = "";
-var repo_url = "https://api.github.com/users/" + username + "/repos"
+var app = angular.module('myapp', []);
+var username = $("#username").val();
 var repos = [];
+var repo_url = "" + username + "";
 
-//A function the places all the repos from a user to a varible
-function repos_from_user (username, repos){
-	$.getJSON(repo_url, function(data, status){
-		$.each(data, function(id, val){
-			repos.push(data[id].name)
-		});
-	});
-};
-
-//A function that places all the varibles to a elerment on a webpage
-function veiwing_repos (a, b, place){
-	repos_from_user(a, b)
-	for (var i = 0; i < b.length; i++){
-		$(place).append("<li><a href=repo/"  + a + "/" + b[i] + "><p>" +     a  + "/" + b[i]  + "</p></a></li>")
-	};
-};
-
-//This is used for the code in the main page
-//repos_from_user(username, repos);
-$("#username_repo").click(function(){
-	console.log("button pressed")
-	username = $("#username").val();
-	veiwing_repos(username, repos, "#repos");
-})
+"https://api.github.com/search/users?q=" + username
