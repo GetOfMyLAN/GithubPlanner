1c509013d6bbedc1ba2e3f0f538419d73936726a
Jack <jack@void.gateway>
2018-04-13 19:11:43 +1000

    adding passwords and stuff

--- public/js/repos.js
+++ public/js/repos.js
@@ -1,6 +1,22 @@
-var app = angular.module('myapp', []);
+//var GitHub = require('github-api');
 var username = $("#username").val();
-var repos = [];
-var repo_url = "" + username + "";
+var password = $("#password").val();
+//Authorization
+$("#login_button").click(function(){
+	$("#login_errors").html("")
+	username = $("#username").val();
+	if (username === ""){
+		$("#login_errors").append("Please enter a <b>username</b><br>")
+		$("#login_errors").fadeIn();
+	}
+	if (password === ""){
+    $("#login_errors").append("Please enter a <b>password</b><br>")
+		$("#login_errors").fadeIn();
+  }
 
-"https://api.github.com/search/users?q=" + username
+	console.log(username)
+});
+//var passwordAuth = new GitHub({
+//  username: username,
+//  password: password
+//});
