87a8ba6a97dbcaaa3426b225b363629fb111a3ce
GitOffMyLAN <jackpsthefirst@gmail.com>
2018-06-28 16:22:11 +1000

    added a writing function

---
+++ public/js/writtingToPlanner.js
@@ -0,0 +1,8 @@
+//-------------
+//||Functions||
+//-------------
+
+//Function that takes a string and a file, and writes it to the end or to the whole file
+function writeToFile(file, stringToBeWritten, options){
+	$.ajax({})
+}
